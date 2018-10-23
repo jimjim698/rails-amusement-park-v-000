b34f69d2e30e23d7fe705ba66e8aced596178cc2
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-23 06:45:03 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,7 +2,18 @@ class RidesController < ApplicationController
 
 
   def create
-
+    ride = Ride.create(ride_params)
+    ride.take_ride
     redirect_to user_path(session[:user_id])
   end
+
+
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:user_id, :attraction_id)
+  end 
+
+  
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,32 +1,32 @@
 class Ride < ActiveRecord::Base
   belongs_to :attraction
-  belongs_to :user 
+  belongs_to :user
 
   def tall_enough
-    self.user.height >= self.attraction.min_height 
+    self.user.height >= self.attraction.min_height
   end
 
   def enough_tickets
-    self.user.tickets >= self.attraction.tickets 
-  end 
+    self.user.tickets >= self.attraction.tickets
+  end
 
   def take_ride
     if tall_enough && enough_tickets
-      ride 
-    elsif 
+      ride
+    elsif
       !tall_enough && enough_tickets
       "Sorry you are not tall enough to ride this ride"
-    elsif 
+    elsif
       tall_enough && !enough_tickets
       "Sorry you do not have enough tickets to ride this ride"
-    end 
-  end 
+    end
+  end
 
-  def ride 
+  def ride
     happiness = self.user.happiness + self.attraction.happiness_rating
     nausea = self.user.nausea + self.attraction.nausea_rating
-    tickets = self.user.tickets - self.attraction.tickets 
+    tickets = self.user.tickets - self.attraction.tickets
     self.user.update(happiness: happiness, nausea: nausea, tickets: tickets)
-    "Thanks for riding the #{self.attraction.name}" 
+    "Thanks for riding the #{self.attraction.name}"
   end      
 end
