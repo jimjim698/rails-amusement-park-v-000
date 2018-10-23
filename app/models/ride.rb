d1519685008e215ee440f1fafad751a1bba1c602
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-23 07:09:55 +0000

    automatically backed up by learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -15,7 +15,6 @@ class Ride < ActiveRecord::Base
     if tall_enough && enough_tickets
       ride
     elsif
-      binding.pry
       !tall_enough && enough_tickets
       "You are not tall enough to ride the #{self.attraction.name}"
     elsif
