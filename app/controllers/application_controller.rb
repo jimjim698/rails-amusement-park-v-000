6a5861a719bdfe4f1c0bd6e7f2d54315bc63d3ad
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-23 03:42:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :verify_user_authenticated
-  skip_before_action :verify_user_authenticated[:home]
+  skip_before_action :verify_user_authenticated, only:[:home]
   helper_method :current_user
 
   def home
