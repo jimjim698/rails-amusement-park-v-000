6bdbc68674f2bc2dc04eec69cea89b94b82976c3
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-23 03:20:12 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :require_login
-  skip_before_action :require_login only:[:new]
+  skip_before_action :require_login, only:[:new]
 
   def new
     @user = User.new
