e59da8a6e1758c84505083afb5ba090f90f95aff
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-23 03:59:19 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   
   def mood
-    if self.happiness > self.nausea
+    if !self.admin && self.happiness > self.nausea
       "happy"
     else 
       "sad"
     end 
   end 
+
+  
 end
