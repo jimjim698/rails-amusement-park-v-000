828638577ab0eb17baf7a3ca9962aa8d59a25399
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-23 05:26:00 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,13 +1,14 @@
 class AttractionsController < ApplicationController
 
   def index
+    binding.pry
     @attractions = Attraction.all
   end
 
 
   def show
     @attraction = Attraction.find(params[:id])
-  end 
+  end
 
 
 
